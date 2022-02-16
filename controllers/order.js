2f8597c7bcb282d20cc1633c1cd1d0cddbe12b84
PNik1ta <75731861+PNik1ta@users.noreply.github.com>
2022-02-16 11:40:44 +0400

    End backend of category, order and position

--- controllers/order.js
+++ controllers/order.js
@@ -1,9 +1,59 @@
-//TODO
-module.exports.getAll = (req, res) => {
+const Order = require('../models/Order');
+const errorHandler = require('../utils/errorHandler');
 
+//(GET) localhost:5000/api/order?offset=2&limit=5
+module.exports.getAll = async (req, res) => {
+	const query = {
+		user: req.user.id
+	};
+
+	//Start date
+	if (req.query.start) {
+		query.date = {
+			$gte: req.query.start //greater or equal
+		}
+	}
+
+	if (req.query.end) {
+		if (!query.date) {
+			query.date = {};
+		}
+		query.date['$lte'] = req.query.end; //less or equal
+	}
+
+	if (req.query.order) {
+		query.order = +req.query.order;
+	}
+
+	try {
+		const orders = await Order
+			.find(query)
+			.sort({ date: -1 })
+			.skip(+req.query.offset)
+			.limit(+req.query.limit);
+
+		res.status(200).json(orders);
+	}
+	catch (error) {
+		errorHandler(res, error);
+	}
 }
 
-//TODO
-module.exports.create = (req, res) => {
+module.exports.create = async (req, res) => {
+	try {
+		const lastOrder = await Order.findOne({ user: req.user.id }).sort({ date: -1 });
 
+		const maxOrder = lastOrder ? lastOrder.order : 0;
+
+		const order = await new Order({
+			list: req.body.list,
+			user: req.user.id,
+			order: maxOrder + 1
+		}).save()
+
+		res.status(201).json(order);
+	}
+	catch (error) {
+		errorHandler(res, error);
+	}
 }
